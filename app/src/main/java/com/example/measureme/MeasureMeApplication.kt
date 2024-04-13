7de9729f6d3ab68323ab6713294395ccf85a6291
Huehueh <kinga.karczewska67@gmail.com>
2024-04-13 13:59:33 +0200

    sending image to server works, need some cleanup

---
+++ app/src/main/java/com/example/measureme/MeasureMeApplication.kt
@@ -0,0 +1,7 @@
+package com.example.measureme
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MeasureMeApplication : Application()
---
+++ app/src/main/java/com/example/measureme/SharingTargetViewModel.kt
@@ -0,0 +1,64 @@
+package com.example.measureme
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Build.VERSION
+import android.os.Bundle
+import android.os.Parcelable
+import android.util.Log
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavController
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import javax.inject.Inject
+
+@HiltViewModel
+class SharingTargetViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle
+) :ViewModel() {
+//    val sharedContentState = savedStateHandle.getStateFlow(NavController.KEY_DEEP_LINK_INTENT, Intent())
+//        .map { intent -> intent.parseSharedContent() }
+//        .stateIn(
+//            scope = viewModelScope,
+//            started = SharingStarted.WhileSubscribed(5_000),
+//            initialValue = ""
+//        )
+}
+
+//fun Intent.parseSharedContent() : String {
+//
+//    Log.i("SharingTargetViewModel", "parseSharedContent $action")
+////    if (action == Intent.ACTION_SEND) {
+////        Log.i("SharingTargetViewModel", "ACTION_SEND")
+//
+//        if(isImageMimeType()) {
+//            Log.i("SharingTargetViewModel", "image")
+////            val imageContent = getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as? Uri
+//            val imageContent = if (VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+//                getParcelableExtra(Intent.EXTRA_STREAM, Uri::class.java)
+//            } else {
+//                getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as? Uri
+//            }
+//            if(imageContent != null)
+//            {
+//                Log.i("SharingTargetViewModel", imageContent.toString())
+//                return imageContent.toString()
+//            }
+//        }
+////    }
+//    return "BRAK"
+//}
+//
+//private const val MIME_TYPE_IMAGE = "image/"
+//private fun Intent.isImageMimeType() = type?.startsWith(MIME_TYPE_IMAGE) == true
+
